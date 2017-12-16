c68726553ec9fa6c1980207c8db8d70d94eeecf1
user <user@boss>
2017-12-16 08:25:33 +0530

    Some modifications

--- countFiles.sh
+++ countFiles.sh
@@ -6,7 +6,7 @@ elif [ "$#" -eq 1 ];then
 	my=0
 	for i in "$1"/*
 	do
-	if [ -f "$i" ];then
+	if [ -f "$i" ] && ! [ -h "$i" ];then
 		let my+=1
 	fi
 	done
@@ -15,7 +15,7 @@ elif [ "$#" -eq 2 ];then
 	my=0
 	for i in "$1"/*
 	do
-	if [ -f "$i" ] && [[ $"$i" == *"$2" ]];then
+	if [ -f "$i" ] && [[ $"$i" == *"$2" ]] && ! [ -h "$i" ];then
 		let my+=1
 	fi
 	done
