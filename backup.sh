28f497a9fda9bc6e85b5fa83882a9a76c8551ee8
AmanTiwari1503 <31160025+AmanTiwari1503@users.noreply.github.com>
2017-12-18 15:58:30 +0530

    Major change in approach

--- backup.sh
+++ backup.sh
@@ -1,48 +1,53 @@
 #!/bin/bash
 #File syncer
-dir1="$1"
-dir2="$2"
 if [ "$#" -ne 2 ]; then
 	exit 1
 fi
-if ! [ -d "$dir1" ] || ! [ -d "$dir2" ]; then
+if ! [ -d "$1" ] || ! [ -d "$2" ]; then
 	exit 1
 fi
-echo "Files copied from $dir1 to $dir2"
+backup_method()
+{
+dir1="$1"
 for i in "$dir1"/*
 do
+	if [ -d "$i" ]; then
+		backup_method "$i" "$2"
+	fi
 	path1="$i"	
-	file1=$(basename $path1)	
-	flag=0
-	for j in "$dir2"/* 
-	do
-		path2="$j"	
-		file2="$(basename $path2)"
-		if [ "$file1" == "$file2" ]; then
-		let flag+=1
-		fi
-	done
+	file1="$(basename $path1)"	
+	sync_check "$file1" "path1" "$2"
+	local flag=$?
 	if [ "$flag" -ne 1 ]; then
-		cp "$i" "$dir2"
+		cp "$i" "$2"
 		echo $file1
 	fi
 done
-echo "Files copied from $dir2 to $dir1"
-for i in "$dir2"/*
+}
+sync_check()
+{
+f=0
+dir2="$3"
+for j in "$dir2"/*
 do
-	path1="$i"	
-	file1="$(basename $path1)"
-	flag=0
-	for j in "$dir1"/* 
-	do
-		path2="$j"	
-		file2="$(basename $path2)"
-		if [ "$file1" == "$file2" ]; then
-		let flag+=1
+	if [ -d "$j" ]; then
+		sync_check "$1" "$2" "$j"
+		local f=$?
+		if [ "$f" -eq 1 ]; then
+		break
 		fi
-	done
-	if [ "$flag" -ne 1 ]; then
-		cp "$i" "$dir1"
-		echo $file1
+	else
+		path2="$j"
+		file2="$(basename $path2)"
+		if [ "$1" == "$file2" ]; then
+			let f+=1
+			break
+		fi
 	fi
 done
+return $f
+}
+echo "Files copied from $1 to $2"
+backup_method "$1" "$2"
+echo "Files copied from $2 to $1"
+backup_method "$2" "$1"
